15c8b284ab7f1f83fd96c60dd572ea4f2a3ce4b1
junkit22 <junkit22@gmail.com>
2024-10-05 12:49:34 +0800

    Updated the lambda function msg

--- .github/workflows/files/lambda_function.py
+++ .github/workflows/files/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    incoming_msg = "good afternoon from us!"
+    incoming_msg = "good afternoon from Stephen, Alif, Dijay kumar, Lai Chua, Jun Jie!"
     return {
         'statusCode': 200,
         'body': json.dumps(incoming_msg)
