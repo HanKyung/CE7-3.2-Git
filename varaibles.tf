8daf9bd1da7de8239e17c5e27372de9ec4e0fbc7
lcchua <laichwang@gmail.com>
2024-10-05 13:31:49 +0800

    format

--- varaibles.tf
+++ varaibles.tf
@@ -1,9 +1,9 @@
 variable "sns_topic_name" {
-  type = string
+  type    = string
   default = "ce7_lcchua-tf-sns1"
 }
 
 variable "sns_endpoint" {
-  type = string
+  type    = string
   default = "laichwang@gmail.com"
 }
